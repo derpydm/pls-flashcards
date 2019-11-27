51212851e37e4ffafd4fc7303c7e26042dcd7720
Sean Wong <derpydm@gmail.com>
2019-11-27 12:53:54 +0800

    finish up UI for start screen

--- pls-flashcards/ViewController.swift
+++ pls-flashcards/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sec1: UIButton!
+    @IBOutlet weak var sec2: UIButton!
+    @IBOutlet weak var sec3: UIButton!
+    @IBOutlet weak var sec4: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let buttons = [sec1,sec2,sec3,sec4]
+        for button in buttons {
+            button!.layer.cornerRadius = 10
+            button!.layer.borderWidth = 1
+        }
         // Do any additional setup after loading the view.
     }
 
